2b887633f24553bc30ba7e384a0897e20abb9e34
Jenny100901 <jason11160@gmail.com>
2025-02-03 06:20:37 +0800

    Add files via upload

---
+++ 0203.py
@@ -0,0 +1,16 @@
+def make_negative(number):
+    return -abs(number)
+Number = int(input())
+
+
+make_negative( Number )
+print("ddd\n")
+
+def smash(words):
+    return ''.join(words)
+words = list(input())
+
+print(smash(words))
+
+
+
