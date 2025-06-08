e2827c1420f3bd361dc7bce4a9581b79edcc0954
Jenny100901 <jason11160@gmail.com>
2025-06-09 03:25:57 +0800

    Add files via upload

---
+++ conn.py
@@ -0,0 +1,45 @@
+print("hello world\n")
+
+
+
+y ='Apple'
+print(f'y= {y}')
+
+print(f'{y=}')
+print(f'{y.upper()= :-^20}')
+number =[1,2,3,4,5]
+number.append(4)
+print(number)
+#append() pop() remove() reverse() sort()
+#____.copy() 複製 list
+#____.extend([10,20,30]) 附加多個元素
+
+#del number[0] 刪掉 位置0  的 元素
+#___________________________________________
+users =set()
+users.add('caterpillar')
+users.add('Justin')
+print(users)
+
+xxy = set([1,2,3])
+yyx =set(tuple([1,2,4,5]))
+print(xxy,"\n")
+print(yyx)
+
+#_______________________________________________
+
+
+passwords = {'Apple':123,'Banana':44564}
+print(passwords['Apple'])
+print(passwords)
+passwords['Canada']=820901
+print(passwords)
+#%%
+
+
+print(123)
+
+
+
+#%%
+
